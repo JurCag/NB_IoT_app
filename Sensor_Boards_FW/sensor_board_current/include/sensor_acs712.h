c439f5a907b0908553b202eb9d56f9657fd8d6ce
Jur Cag <200349@vutbr.cz>
2023-03-11 10:36:01 +0100

    Calibrate conversion from mV to current.

--- Sensor_Boards_FW/sensor_board_current/include/sensor_acs712.h
+++ Sensor_Boards_FW/sensor_board_current/include/sensor_acs712.h
@@ -15,9 +15,17 @@
 // Sensor output voltage (mV) at 0A current
 #define ACS712_0A_OUTPUT_VOLTAGE        (2307.0)
 
-// Voltage divieder R1 = 1k, R2 = 2k
+// Voltage divieder R1 = 100k, R2 = 100k
 // ADC voltage measured on R2 -> ratio = R2/(R1 + R2)
-#define VOLTAGE_DIVIDER_RATIO       (2.0/3.0) //(2.0/12.0)
+#define VOLTAGE_DIVIDER_RATIO           (1.0/2.0)
+
+// Manual callibration (linear, y = a * x + b)
+// y ... real (measured/known) current
+// x ... uncallibrated current (from ESP)
+// a ... linear gain
+// b ... linear offset
+#define LIN_CALIB_GAIN                  (1.037)
+#define LIN_CALIB_OFFSET                (0.34641)
 
 void sensorAcs712CreateTaskDataAcq(void);
 float sensorAcs712GetCurrent(void);
--- Sensor_Boards_FW/sensor_board_current/src/sensor_acs712.c
+++ Sensor_Boards_FW/sensor_board_current/src/sensor_acs712.c
@@ -4,6 +4,10 @@ static void taskDataAcq(void *pvParameters);
 static uint32_t mV;
 static uint32_t rawVal;
 
+static uint32_t mvSum;
+static uint32_t mvAvg;
+static uint8_t cnt = 0;
+
 void sensorAcs712CreateTaskDataAcq(void)
 {
     xTaskCreate(
@@ -20,14 +24,22 @@ static void taskDataAcq(void *pvParameters)
 {
     if (adcConfig() == EXIT_SUCCESS)
     {
-
         while(1)
         {
-            vTaskDelay(500/portTICK_PERIOD_MS);
+            vTaskDelay(25/portTICK_PERIOD_MS);
+            
             mV = adcGetVoltage();
             rawVal = adcGetRawValue();
 
-            printf("Voltage: %d mV, raw value: %d, current: %.2f A\n", mV, rawVal, sensorAcs712GetCurrent());
+            cnt++;
+            mvSum += mV;
+            if (cnt % 40 == 0)
+            {
+                mvAvg = mvSum / cnt;
+                printf("Voltage: %d mV, raw value: %d, current: %.2f A\n", mvAvg, rawVal, sensorAcs712GetCurrent());
+                cnt = 0;
+                mvSum = 0;
+            }
         }
     }
 
@@ -38,5 +50,10 @@ static void taskDataAcq(void *pvParameters)
 
 float sensorAcs712GetCurrent(void)
 {
-    return ( (((mV / VOLTAGE_DIVIDER_RATIO) - ACS712_0A_OUTPUT_VOLTAGE) / ACS712_SENSITIVITY)); // A
+    static float theoreticalVal;
+    static float linCalibratedVal;
+    theoreticalVal = (((float)mvAvg / VOLTAGE_DIVIDER_RATIO) - ACS712_0A_OUTPUT_VOLTAGE) / ACS712_SENSITIVITY;
+    linCalibratedVal = (1.0 / LIN_CALIB_GAIN) * theoreticalVal - LIN_CALIB_OFFSET;
+    return linCalibratedVal;
+    // return ((((float)mvAvg) * 0.02074f) - 24.27);
 }
